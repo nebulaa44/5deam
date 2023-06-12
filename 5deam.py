94c0103cfbd32bf04da18a774c91587370fa764d
nebulaa44 <gmdmir372@gmail.com>
2023-06-12 11:57:41 -0500

    actually get level id from the user

--- 5deam.py
+++ 5deam.py
@@ -1,5 +1,13 @@
+import argparse
 import requests
 
-lvl_id = 89
+parser = argparse.ArgumentParser(
+    prog="5deam",
+    description="5beam client")
+
+parser.add_argument("-l", "--level")
+args = parser.parse_args()
+
+lvl_id = int(args.level)
 response = requests.get("https://5beam.zelo.dev/api/level?id=%i" % lvl_id)
 print(response.text)
