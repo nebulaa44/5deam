011ae1c0d6cfbb692961c10d5602cb800a2bdede
nebulaa44 <gmdmir372@gmail.com>
2023-06-15 20:26:44 -0500

    add cli support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 mod api;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args
+{
+    #[arg(short, long)]
+    id: i64,
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<() ,Box<dyn std::error::Error>>
 {
-    let resp = api::get_level(89).await?;
+    let args = Args::parse();
+
+    let resp = api::get_level(args.id).await?;
     println!("{resp}");
 
     Ok(())
