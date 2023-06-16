5df89f1457777e03242f7b412c9fc0c25a8465ab
nebulaa44 <gmdmir372@gmail.com>
2023-06-16 14:09:04 -0500

    add option to make a new levels.txt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod api;
+mod init_levels;
 
 use clap::Parser;
 
@@ -6,17 +7,31 @@ use clap::Parser;
 #[command(author, version, about)]
 struct Args
 {
-    #[arg(short, long)]
+    //? These should probably be subcommands
+
+    /// Creates a new levels.txt at the given path
+    #[arg(short, long, default_value_t = format!(""))]
+    generate_levels: String,
+
+    /// Level ID to download
+    #[arg(short, long, default_value_t = 0)]
     id: i64,
 }
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<() ,Box<dyn std::error::Error>>
+async fn main() -> Result<(), Box<dyn std::error::Error>>
 {
     let args = Args::parse();
 
-    let resp = api::get_level(args.id).await?.data;
-    println!("{resp}");
+    if args.generate_levels != "" 
+    {
+        init_levels::init_levels();
+    }
 
+    if args.id != 0
+    {
+        let resp = api::get_level(args.id).await?.data;
+        println!("{resp}");
+    }
     Ok(())
 }
